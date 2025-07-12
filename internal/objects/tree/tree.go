49e52959336a5bec7d555303ac4ddfdf83249cfc
Grishma Dhakal <dhakalgrishma5@gmail.com>
2025-07-12 11:12:23 +0545

    feat(tree): implement Tree object with AddEntry and basic serialization (need to update later)

---
+++ internal/objects/tree/tree.go
@@ -0,0 +1,106 @@
+package tree
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/Gr1shma/notgit/internal/objects"
+)
+
+type EntryType int
+
+const (
+	EntryTypeBlob EntryType = iota
+	EntryTypeTree
+)
+
+type Entry struct {
+	Name string
+	Hash string
+	Type EntryType
+}
+
+type Tree struct {
+	Entries  []Entry
+	entryMap map[string]*Entry
+	Hash     string
+}
+
+var _ objects.Object = (*Tree)(nil)
+
+func NewTree() *Tree {
+	return &Tree{
+		Entries:  make([]Entry, 0),
+		entryMap: make(map[string]*Entry),
+	}
+}
+
+func (t *Tree) Type() string {
+	return "tree"
+}
+
+func (t *Tree) AddEntry(name, hash string, entryType EntryType) {
+	if existingEntry, exists := t.entryMap[name]; exists {
+		existingEntry.Hash = hash
+		existingEntry.Type = entryType
+		t.Hash = ""
+		return
+	}
+
+	entry := Entry{
+		Name: name,
+		Hash: hash,
+		Type: entryType,
+	}
+	t.Entries = append(t.Entries, entry)
+	t.entryMap[name] = &t.Entries[len(t.Entries)-1] // Point to the actual entry in slice
+
+	t.Hash = ""
+}
+
+func (t *Tree) Serialize() ([]byte, error) {
+	var buffer bytes.Buffer
+	for _, entry := range t.Entries {
+		mode := t.getModeForType(entry.Type)
+		typeStr := t.getTypeString(entry.Type)
+
+		line := fmt.Sprintf("%s %s %s\t%s\n",
+			mode,
+			typeStr,
+			entry.Hash,
+			entry.Name,
+		)
+		buffer.WriteString(line)
+	}
+	return buffer.Bytes(), nil
+}
+
+func DeserializeTree(data []byte) (*Tree, error) {
+	return nil, nil
+}
+
+func (t *Tree) GetHash() string {
+	return t.Hash
+}
+
+func (t *Tree) getModeForType(entryType EntryType) string {
+	switch entryType {
+	case EntryTypeBlob:
+		return "100644"
+	case EntryTypeTree:
+		return "040000"
+	default:
+		return "100644"
+	}
+}
+
+func (t *Tree) getTypeString(entryType EntryType) string {
+	switch entryType {
+	case EntryTypeBlob:
+		return "blob"
+	case EntryTypeTree:
+		return "tree"
+	default:
+		return "blob"
+	}
+}
