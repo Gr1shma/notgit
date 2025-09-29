b6b0ecb39d845e9cf4959de26d8ed7cb17a59523
Grishma Dhakal <dhakalgrishma5@gmail.com>
2025-09-29 16:00:56 +0545

    fix(commands/add): relative path error and accept many args

--- internal/commands/add.go
+++ internal/commands/add.go
@@ -14,86 +14,99 @@ import (
 var addVerboseBool bool
 
 var addCmd = &cobra.Command{
-	Use:   "add [path]",
+	Use:   "add <pathspec>...",
 	Short: "Add file contents to the index",
 	Long:  `Add file contents to the index by storing them as blob objects in .notgit/objects.`,
 	Args:  cobra.MinimumNArgs(1),
-
-	Run: addCallback,
+	RunE:  addCallback,
 }
 
 func init() {
-	addCmd.PersistentFlags().BoolVarP(&addVerboseBool, "verbose", "v", false, "Use verbose output to show added files")
+	addCmd.PersistentFlags().BoolVarP(&addVerboseBool, "verbose", "v", false, "Be verbose and show files as they are added")
 	rootCmd.AddCommand(addCmd)
 }
 
-func addCallback(cmd *cobra.Command, args []string) {
-	if len(args) < 1 {
-		fmt.Println("Please specify a file or directory to add.")
-		return
-	}
-
+func addCallback(cmd *cobra.Command, args []string) error {
 	repo, err := repository.OpenRepository(".")
 	if err != nil {
-		fmt.Println("Error: not inside a notgit repository.")
-		return
+		return fmt.Errorf("failed to open repository: %w", err)
 	}
 
 	index, err := repo.LoadIndex()
 	if err != nil {
-		fmt.Printf("Error loading index: %v\n", err)
-		return
+		return fmt.Errorf("failed to load index: %w", err)
 	}
 
-	rootPath := args[0]
+	repoRoot := filepath.Dir(repo.NotgitDir)
 
-	err = filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+	for _, pathSpec := range args {
+		if _, err := os.Stat(pathSpec); os.IsNotExist(err) {
+			return fmt.Errorf("pathspec '%s' did not match any files", pathSpec)
 		}
-		if info.IsDir() {
-			if path == repo.NotgitDir {
-				return filepath.SkipDir
+
+		err := filepath.Walk(pathSpec, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
 			}
+
+			absPath, err := filepath.Abs(path)
+			if err != nil {
+				return fmt.Errorf("could not get absolute path for %s: %w", path, err)
+			}
+			absNotgitDir, err := filepath.Abs(repo.NotgitDir)
+			if err != nil {
+				return fmt.Errorf("could not get absolute path for repository dir: %w", err)
+			}
+
+			if strings.HasPrefix(absPath, absNotgitDir) {
+				if info.IsDir() {
+					return filepath.SkipDir
+				}
+				return nil
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			data, err := os.ReadFile(path)
+			if err != nil {
+				return fmt.Errorf("failed to read file %s: %w", path, err)
+			}
+
+			b, err := blob.NewBlob(data)
+			if err != nil {
+				return fmt.Errorf("failed to create blob for %s: %w", path, err)
+			}
+
+			hash, err := repo.StoreObject(b)
+			if err != nil {
+				return fmt.Errorf("failed to store object for %s: %w", path, err)
+			}
+
+			relativePath, err := filepath.Rel(repoRoot, absPath)
+			if err != nil {
+				return fmt.Errorf("failed to determine repository-relative path for %s: %w", path, err)
+			}
+			relativePath = filepath.ToSlash(relativePath)
+
+			index.AddEntry(relativePath, hash)
+
+			if addVerboseBool {
+				fmt.Fprintf(cmd.OutOrStdout(), "add '%s'\n", relativePath)
+			}
+
 			return nil
-		}
+		})
 
-		absNotgit, _ := filepath.Abs(repo.NotgitDir)
-		absPath, _ := filepath.Abs(path)
-		if strings.HasPrefix(absPath, absNotgit+string(os.PathSeparator)) || absPath == absNotgit {
-			return nil
-		}
-
-		data, err := os.ReadFile(path)
 		if err != nil {
-			return fmt.Errorf("failed to read %s: %w", path, err)
+			return fmt.Errorf("error processing path '%s': %w", pathSpec, err)
 		}
-
-		b, err := blob.NewBlob(data)
-		if err != nil {
-			return fmt.Errorf("failed to create blob for %s: %w", path, err)
-		}
-
-		hash, err := repo.StoreObject(b)
-		if err != nil {
-			return fmt.Errorf("failed to store object for %s: %w", path, err)
-		}
-
-		rel, _ := filepath.Rel(".", path)
-		index.AddEntry(rel, hash)
-
-		if addVerboseBool {
-			fmt.Printf("added %s (%s)\n", rel, hash)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		fmt.Printf("error while adding: %v\n", err)
-		return
 	}
+
 	if err := repo.SaveIndex(index); err != nil {
-		fmt.Printf("error saving the index: %s", err)
+		return fmt.Errorf("failed to save the index: %w", err)
 	}
+
+	return nil
 }
